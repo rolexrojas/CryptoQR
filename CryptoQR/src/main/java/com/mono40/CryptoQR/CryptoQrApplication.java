cef712fa155e9db7eb1b10ea453745f37773db09
Roberto Rojas <robertorojas20@gmail.com>
2019-08-11 10:30:30 -0400

    Skeleton App
    initial commit

---
+++ CryptoQR/src/main/java/com/mono40/CryptoQR/CryptoQrApplication.java
@@ -0,0 +1,13 @@
+package com.mono40.CryptoQR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoQrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoQrApplication.class, args);
+	}
+
+}
